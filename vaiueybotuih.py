859336887fb85f1d29568be038917a2e0f136702
16808910041680 <forforfirefire@gmail.com>
2025-03-30 10:49:47 +1100

    hii

---
+++ vaiueybotuih.py
@@ -0,0 +1,16 @@
+a=int(input("Random Number Please"))
+b=int(input("Confirm Your Random Number Please"))
+c=int(input("Actually, Don't Confirm Your Random Number Please"))
+d=int(input("Actually, Just Type Another Random Number Please"))
+e=int(input("Give Me One More Random Number Please"))
+f=int(input("I Know That's Way Too Many Random Numbers, But Again, Random Number Please"))
+g=int(input("That's Enough, But One More Random Number Please"))
+h=int(input("Or You Could Repeat The Same Number For Our 'Random Number Please'"))
+print ("I think that's enough 'RANDOM NUMBER PLEASE', so please do NOT type more random numbers.")
+i= a+b+c+d+e+f+g+h
+j = i/i
+print (a,b,c,d,e,f,g,h)
+if i < j:
+    print ("You have acheived a low number!! Or did you just type 0 all the way through...") 
+else: 
+    print ("<<<^^^")
